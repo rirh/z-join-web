b2fa32820f5cbe66339eb6490f6dedfbfe6d03f5
wenzhi <willwenzhi@gmail.com>
2020-09-15 18:51:53 +0800

    await

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,8 +6,8 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
 const service = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    // baseURL: process.env.REACT_APP_BASE_URL,
-    baseURL: process.env.REACT_APP_HOST_URL,
+    baseURL: process.env.REACT_APP_BASE_URL,
+    // baseURL: process.env.REACT_APP_HOST_URL,
     // 超时
     timeout: 10000
   })
