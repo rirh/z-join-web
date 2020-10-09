23fa5fcf6856a6a5e2096ff46ac7cacfead45057
wenzhi <willwenzhi@gmail.com>
2020-10-09 23:25:07 +0800

    await

---
+++ shell.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec;
+const commend = 'lsof -i tcp:8080';
+const async_exec = (command) => {
+    return new Promise((reslove, reject) => {
+        exec(command, (err, stdout) => {
+            if (err) reject(err);
+            else reslove(stdout)
+        })
+    })
+}
+
+async_exec(commend).then(
+    (stdout) => {
+        console.log(stdout);
+        let port = 0;
+        const line = stdout.split('\n').find((line) => {
+            [, port] = line.trim().split(/\s+/);
+            return !isNaN(Number(port))
+        });
+        console.log(stdout);
+        [, port] = (line.trim().split(/\s+/));
+        port = Number(port);
+        async_exec(`kill -9 ${port}`).then((killstrout) => {
+            console.log(killstrout)
+        })
+    }
+).catch((err) => {
+    console.log(err)
+    console.log(1)
+    
+
+})
